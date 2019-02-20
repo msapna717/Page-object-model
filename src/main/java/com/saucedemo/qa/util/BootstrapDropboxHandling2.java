3c2187a88fe2dbc0ee4f9e1d17088c15064e1bde
Sapna Mishra <msapna717@gmail.com>
2019-02-20 18:22:47 +0530

    adding some util programs

---
+++ src/main/java/com/saucedemo/qa/util/BootstrapDropboxHandling2.java
@@ -0,0 +1,41 @@
+package com.saucedemo.qa.util;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BootstrapDropboxHandling2 {
+	static WebDriver driver;
+
+	@Test
+	public static void test() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "/Users/sapnamishra/Documents/WebDriver/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://seleniumpractise.blogspot.com/2016/08/bootstrap-dropdown-example-for-selenium.html");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.findElement(By.id("menu1")).click();
+		List<WebElement> dd_list = driver.findElements(By.xpath("//ul[@class='dropdown-menu']//li//a"));
+		System.out.println("list size is: " + dd_list.size());
+
+		for (int i = 0; i < dd_list.size(); i++) {
+			WebElement element = dd_list.get(i);
+			String innerHTML = element.getAttribute("innerHTML");
+			if (innerHTML.contains("JavaScript")) {
+				element.click();
+				break;
+			}
+			System.out.println("Values from dropdown is====== " + innerHTML);
+
+		}
+		Thread.sleep(3000);
+		driver.quit();
+
+	}
+
+}
